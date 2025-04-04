dc4838eca1e54fb5896ecfe3b4dc5e31f8d07c22
shakk1l1 <thibault.fesler@gmail.com>
2025-04-04 10:33:00 +0200

    error handler

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ import joblib
 from database import *
 from data_extract import *
 from POD import *
+try:
+    from path import path
+
+    pathtest = path
+except Exception as e:
+    print("path file not found")
+    print("Creating path file...")
+    new_path = input('path to data folder (finish with .../AicraftSpotter/data):')
+    with open('command_general.py', 'w') as f:
+        f.write('path = ' + "'" + new_path + "'")
+    print("Path created")
+    from path import path
 
 ## Main function
 def main():
     print("Welcome to the AI Craft Spotter")
-    try:
-        from path import path
-        pathtest = path
-    except Exception as e:
-        print("path file not found")
-        print("Creating path file...")
-        new_path = input('path to data folder (finish with .../AicraftSpotter/data):')
-        with open('command_general.py', 'w') as f:
-            f.write('path = ' + "'" + new_path + "'")
-        print("Path created")
-        from path import path
-
     try:
         print("Connecting to database...")
         db.get_connection()
